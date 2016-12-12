404c9bb0dcf0da6c249a93e7900fd8f0b84e6897
radoslav.latincic <latincic@teol.net>
2016-12-12 15:04:59 +0100

    Primjer 2

---
+++ Radoslav02-Tipovi Podataka/src/App.java
@@ -0,0 +1,23 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int myNumber=99;
+		short myShort=987;
+		char myChar='r';
+		boolean myTrue=true;
+		byte myByte=127;
+		
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myChar);
+		System.out.println(myTrue);
+		System.out.println(myByte);
+		
+		
+	}
+
+}
