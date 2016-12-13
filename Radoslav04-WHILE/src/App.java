172f5a5fdbf2c76b0a6fe1427227faaeea197e26
radoslav.latincic <latincic@teol.net>
2016-12-13 11:02:34 +0100

    Primjer 4

---
+++ Radoslav04-WHILE/src/App.java
@@ -0,0 +1,31 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=0;
+while(i<100)
+{
+	System.out.println(i);
+i++;	
+}
+
+ i=30;
+while(i<50)
+{
+	System.out.println(i);
+i++;	
+}
+
+i=50;
+while(i>30)
+{
+	System.out.println(i);
+i--;	
+}
+
+	
+	}	
+
+
+}
